e41ff08a7148a6bc8f99b62918fd1c924564966c
s183478 <s183478@student.dtu.dk>
2020-06-16 12:06:46 +0200

    Initial commit of CreateRouteActivity.java

---
+++ app/src/main/java/com/example/routeplanner/CreateRouteActivty.java
@@ -0,0 +1,14 @@
+package com.example.routeplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateRouteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
