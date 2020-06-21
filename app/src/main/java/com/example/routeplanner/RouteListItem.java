a8c280361bea77b8a5d2078e11ab91ea6b31a527
Zebzab7 <56087724+Zebzab7@users.noreply.github.com>
2020-06-21 16:39:14 +0200

    Added extra fields to RouteListItem

--- app/src/main/java/com/example/routeplanner/RouteListItem.java
+++ app/src/main/java/com/example/routeplanner/RouteListItem.java
@@ -1,5 +1,10 @@
 package com.example.routeplanner;
 
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.Polyline;
+
+import java.util.Stack;
+
 public class RouteListItem {
 
     private int id;
@@ -8,11 +13,39 @@ public class RouteListItem {
     private String completions;
     private String avgSpeed;
 
+    private Stack<Marker> markerStack = new Stack<Marker>();
+    private Stack<Polyline> polylineStack = new Stack<Polyline>();
+    private Polyline cycleLine;
+
     public RouteListItem(String name, String distance, String completions, String avgSpeed) {
         this.name = name;
         this.distance = distance;
         this.completions = completions;
         this.avgSpeed = avgSpeed;
+    }
+
+    public Stack<Marker> getMarkerStack() {
+        return markerStack;
+    }
+
+    public void setMarkerStack(Stack<Marker> markerStack) {
+        this.markerStack = markerStack;
+    }
+
+    public Stack<Polyline> getPolylineStack() {
+        return polylineStack;
+    }
+
+    public void setPolylineStack(Stack<Polyline> polylineStack) {
+        this.polylineStack = polylineStack;
+    }
+
+    public Polyline getCycleLine() {
+        return cycleLine;
+    }
+
+    public void setCycleLine(Polyline cycleLine) {
+        this.cycleLine = cycleLine;
     }
 
     public int getId() {
