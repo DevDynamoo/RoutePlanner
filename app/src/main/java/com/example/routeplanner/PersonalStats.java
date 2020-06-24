7004997f92586efea4980e626116e0cb56b796df
montymse <mse450@hotmail.com>
2020-06-24 15:09:23 +0200

    Clean up in PersonalStats

--- app/src/main/java/com/example/routeplanner/PersonalStats.java
+++ app/src/main/java/com/example/routeplanner/PersonalStats.java
@@ -10,32 +10,17 @@ public class PersonalStats {
         return totalspeed;
     }
 
-    public void setTotalspeed(float totalspeed) {
-        this.totalspeed = totalspeed;
-    }
-
     public int getNum() {
         return num;
-    }
-
-    public void setNum(int num) {
-        this.num = num;
     }
 
     public float getTotaldistance() {
         return totaldistance;
     }
 
-    public void setTotaldistance(float totaldistance) {
-        this.totaldistance = totaldistance;
-    }
-
     public int getTime() {
         return time;
     }
 
-    public void setTime(int time) {
-        this.time = time;
-    }
 }
 
